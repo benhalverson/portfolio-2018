9511fb9ae6b7473786874736285261c29d0bbdf9
Ben Halverson <benhalverson33@gmail.com>
2018-09-03 20:43:44 -0700

    reactstrap and updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Nav, NavItem } from 'reactstrap';
+
 const Header = () => (
   <header>
     <h1>Portfolio header</h1>
     <nav>
-      <ul>
-        <li><NavLink to="/" activeClassName="is-active" exact={true}>About</NavLink></li>
-        <li><NavLink to="/portfolio" activeClassName="is-active">Portfolio</NavLink></li>
-        <li><NavLink to="/blog" activeClassName="is-active">Blog</NavLink></li>
-        <li><NavLink to="/contact" activeClassName="is-active">Contact</NavLink></li>
-      </ul>
+      <NavLink to="/" activeClassName="is-active" exact={true}>
+        About
+      </NavLink>
+
+      <NavLink to="/portfolio" activeClassName="is-active">
+        Portfolio
+      </NavLink>
+      
+      <NavLink to="/blog" activeClassName="is-active">
+        Blog
+      </NavLink>
+        
+      <NavLink to="/contact" activeClassName="is-active">
+        Contact
+      </NavLink>
     </nav>
   </header>
-)
+);
 
 export default Header;
